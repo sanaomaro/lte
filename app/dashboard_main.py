8bbe734e68bd0e7203582b5a378dd6a75f4b56ca
sanaomaro <89298616+sanaomaro@users.noreply.github.com>
2026-05-07 14:37:44 +0200

    Update dashboard_main.py

--- app/dashboard_main.py
+++ app/dashboard_main.py
@@ -1,9 +1,9 @@
 
 import streamlit as st
 
-from app.data_processing import load_data, clean_data
-from app.visualization import summarize_data, generate_charts
-from app.modeling import detect_outliers, regression_analysis, predict_snr
+from data_processing import load_data, clean_data
+from visualization import summarize_data, generate_charts
+from modeling import detect_outliers, regression_analysis, predict_snr
 
 
 
